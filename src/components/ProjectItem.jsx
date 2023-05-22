fe94878fe3f4ca08e66f8393df56b0f7513854b9
Shawn Brown <shawnbrownmediabusiness@gmail.com>
2023-05-21 23:51:00 -0400

    Update ProjectItem.jsx

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-function ProjectItem({img, title}) {
+function ProjectItem({ img, title }) {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#043797]'>
-        <img src={img} alt="/" className='rounded-xl group-hover:opacity-10'/>
-        <div className='hiden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-            <h3 className='text-2xl font-bold text-white tracking-wider text-center'>
-                {title}
-            </h3>
-            <p className='pb-4 pt-2 text-white text-center'>React Js</p>
-            <div class="grid grid-cols-2 gap-4">
-                <a href='Code' class="text-center p-2 rounded-lg bg-white text-gray-700 font-blod cursor-pointer text-lg">Code</a>
-                <a href='Demo' class="text-center p-2 rounded-lg bg-white text-gray-700 font-blod cursor-pointer text-lg">Demo</a>
-          </div>
+      <img src={img} alt="/" className='rounded-xl group-hover:opacity-10' />
+      <div className='hidden group-hover:block absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2'>
+        <h3 className='text-2xl font-bold text-white tracking-wider text-center'>
+          {title}
+        </h3>
+        <p className='pb-4 pt-2 text-white text-center'>React Js</p>
+        <div className="grid grid-cols-2 gap-4">
+          <a href='/' className="text-center p-2 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg">Code</a>
+          <a href='/' className="text-center p-2 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg">Demo</a>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ProjectItem;
+
