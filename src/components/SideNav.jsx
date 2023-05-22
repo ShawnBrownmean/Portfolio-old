a3453e9af715d965f9800c3271beabc6d9609b6a
Shawn Brown <shawnbrownmediabusiness@gmail.com>
2023-05-21 23:51:07 -0400

    Update SideNav.jsx

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -20,31 +20,31 @@ function SideNav() {
         <div className='fixed w-full h-screen bg-white/99 flex flex-col justify-center items-center z-20'>
           <a onClick={handleNav}
             href='#main'
-            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-on duration-200'
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg text-white bg-[#3f4b61] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-on duration-200'
           >
             <AiOutlineHome size={20} />
             <span className='pl-4'> home </span>
           </a>
           <a onClick={handleNav}
             href='#work'
-            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-on duration-200'
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg text-white bg-[#3f4b61] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-on duration-200'
           >
             <GrProjects size={20} />
             <span className='pl-4'> Work </span>
           </a>
           <a onClick={handleNav}
             href='#projects'
-            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-on duration-200'
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg text-white bg-[#3f4b61] m-2 p-4 cursor-pointer hover:scale-110 ease-on duration-200'
           >
             <AiOutlineProject size={20} />
-            <span className='pl-4'> Projects </span>
+            <span className='pl-4'> projects </span>
           </a>
           <a onClick={handleNav}
             href='#contact'
-            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-on duration-200'
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg text-white bg-[#3f4b61] m-2 p-4 cursor-pointer hover:scale-110 ease-on duration-200'
           >
             <AiOutlineMail size={20} />
-            <span className='pl-4'> Contact </span>
+            <span className='pl-4'> contact </span>
           </a>
         </div>
       ) : (
@@ -52,16 +52,16 @@ function SideNav() {
       )} 
         <div className='md:block hidden fixed top-[25%] z-10'>
             <div className='flex flex-col'>
-                <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <a href='#main' className='rounded-full shadow-lg text-white bg-[#3f4b61] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                     <AiOutlineHome size={20}/>
                 </a>
-                <a href='#work' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <a href='#work' className='rounded-full shadow-lg text-white bg-[#3f4b61] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                     <GrProjects size={20}/>
                 </a>
-                <a href='#projects' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <a href='#projects' className='rounded-full shadow-lg text-white bg-[#3f4b61] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                     <AiOutlineProject size={20}/>
                 </a>
-                <a href='#contact' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <a href='#contact' className='rounded-full shadow-lg text-white bg-[#3f4b61] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                     <AiOutlineMail size={20}/>
                 </a>
             </div>
