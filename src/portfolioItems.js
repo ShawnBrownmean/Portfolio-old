fde8423442d23f9732b571f6ed1f7325e66fc16f
Mike Melton <michaelcmelton93@gmail.com>
2023-05-23 09:31:12 -0400

    Update portfolioItems.js

--- src/portfolioItems.js
+++ src/portfolioItems.js
@@ -3,7 +3,7 @@ import portfolio1 from './assets/portfolio1.jpg`;
 const items = [{
   image: portfolio1, 
   title: "Portfolio 1", 
-  codeLink: 'https://github.com/ShawnBrownmean/Portfolio.git',
+  codeLink: "https://github.com/ShawnBrownmean/Portfolio.git",
   demoLink: 'https://demo-link-portfolio1.com'
 }]
 
