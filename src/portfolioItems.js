f72d488c966c8fdea3ab9efe1e4bc9c5775d74b2
Mike Melton <michaelcmelton93@gmail.com>
2023-05-23 09:26:39 -0400

    Create portfolioItems.js

---
+++ src/portfolioItems.js
@@ -0,0 +1,10 @@
+import portfolio1 from './assets/portfolio1.jpg`;
+
+const items = [{
+  image: portfolio1, 
+  title: "Portfolio 1", 
+  codeLink: 'https://github.com/ShawnBrownmean/Portfolio.git',
+  demoLink: 'https://demo-link-portfolio1.com'
+}]
+
+export default items;
